a603bbf93e99e7a8c71726eb1ca294b6f28ba932
CASL0 <gin.0046.822@gmail.com>
2022-09-11 12:56:01 +0900

    リポジトリ層を追加

---
+++ app/src/main/java/io/github/casl0/jvnlookup/repository/JvnRepository.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 CASL0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.casl0.jvnlookup.repository
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import io.github.casl0.jvnlookup.database.JvnDatabase
+import io.github.casl0.jvnlookup.database.asDomainModel
+import io.github.casl0.jvnlookup.model.DomainVulnOverview
+import io.github.casl0.jvnlookup.network.MyJvnApi
+import io.github.casl0.jvnlookup.network.asDatabaseCVSS
+import io.github.casl0.jvnlookup.network.asDatabaseReferences
+import io.github.casl0.jvnlookup.network.asDatabaseVulnOverviews
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/**
+ * JVN APIからデータを取得し、Roomに保存するリポジトリ
+ * @param database 保存先のデータベース
+ */
+class JvnRepository(private val database: JvnDatabase) {
+    companion object {
+        private const val TAG = "JvnRepository"
+    }
+
+    val vulnOverviews: LiveData<List<DomainVulnOverview>> = Transformations.map(
+        database.vulnOverviewDao.getVulnOverviewWithReferencesAndCVSS()
+    ) {
+        it.asDomainModel()
+    }
+
+    /**
+     * ローカルに保存しているJVNデータを更新します
+     */
+    suspend fun refreshVulnOverviews() = withContext(Dispatchers.IO) {
+        Log.d(TAG, "refresh vuln overviews")
+        val vulnOverviews = MyJvnApi.retrofitService.getVulnOverviewList()
+        database.vulnOverviewDao.insertAll(vulnOverviews.asDatabaseVulnOverviews())
+        database.referenceDao.insertAll(vulnOverviews.asDatabaseReferences())
+        database.cvssDao.insertAll(vulnOverviews.asDatabaseCVSS())
+    }
+}
