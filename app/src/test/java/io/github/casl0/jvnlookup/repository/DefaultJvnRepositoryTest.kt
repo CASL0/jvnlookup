bae04fc7028d589e88de141e33eb1478ffcc45c2
CASL0 <gin.0046.822@gmail.com>
2023-11-02 22:38:21 +0900

    test: DefaultJvnRepository::refreshVulnOverviewsのローカルデータ更新のテスト

--- app/src/test/java/io/github/casl0/jvnlookup/repository/DefaultJvnRepositoryTest.kt
+++ app/src/test/java/io/github/casl0/jvnlookup/repository/DefaultJvnRepositoryTest.kt
@@ -16,16 +16,96 @@
 
 package io.github.casl0.jvnlookup.repository
 
+import io.github.casl0.jvnlookup.data.FakeJvnDataSource
 import io.github.casl0.jvnlookup.data.JvnDataSource
+import io.github.casl0.jvnlookup.model.DomainVulnOverview
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class DefaultJvnRepositoryTest {
+    private lateinit var initialSavedLocalVulnOverviews: List<DomainVulnOverview>
+    private lateinit var stubRemoteVulnOverviews: List<DomainVulnOverview>
+    private lateinit var dataSource: JvnDataSource
+
+    @Before
+    fun setup() {
+        initialSavedLocalVulnOverviews = listOf(
+            DomainVulnOverview(
+                title = "D-Link Systems, Inc. の DIR-846 ファームウェアにおける脆弱性",
+                link = "https://jvndb.jvn.jp/ja/contents/2020/JVNDB-2020-017892.html",
+                description = "D-Link Systems, Inc. の DIR-846 ファームウェアには、不特定の脆弱性が存在します。",
+                id = "JVNDB-2020-017892",
+                issued = "2023-10-30T17:23:20+09:00",
+                modified = "2023-10-30T17:23:20+09:00",
+                isFavorite = true
+            ),
+            DomainVulnOverview(
+                title = "Bluetooth SIG, Inc. の Bluetooth Core Specification における観測可能な不一致に関する脆弱性",
+                link = "https://jvndb.jvn.jp/ja/contents/2020/JVNDB-2020-017895.html",
+                description = "Bluetooth SIG, Inc. の Bluetooth Core Specification には、観測可能な不一致に関する脆弱性、Capture-replay による認証回避に関する脆弱性が存在します。",
+                id = "JVNDB-2020-017895",
+                issued = "2023-11-02T17:07:04+09:00",
+                modified = "2023-11-02T17:07:04+09:00",
+                isFavorite = false
+            ),
+            DomainVulnOverview(
+                title = "Google の Android における境界外書き込みに関する脆弱性",
+                link = "https://jvndb.jvn.jp/ja/contents/2022/JVNDB-2022-020636.html",
+                description = "Google の Android には、境界外書き込みに関する脆弱性が存在します。",
+                id = "JVNDB-2022-020636",
+                issued = "2023-11-02T17:07:02+09:00",
+                modified = "2023-11-02T17:07:02+09:00",
+                isFavorite = false
+            )
+        )
+
+        stubRemoteVulnOverviews = listOf(
+            DomainVulnOverview(
+                title = "シトリックス・システムズの Citrix Gateway および Citrix Application Delivery Controller ファームウェアにおける過度な認証試行の不適切な制限に関する脆弱性",
+                link = "https://jvndb.jvn.jp/ja/contents/2022/JVNDB-2022-020610.html",
+                description = "シトリックス・システムズの Citrix Gateway および Citrix Application Delivery Controller ファームウェアには、過度な認証試行の不適切な制限に関する脆弱性が存在します。",
+                id = "JVNDB-2022-020610",
+                issued = "2023-11-02T17:06:01+09:00",
+                modified = "2023-11-02T17:06:01+09:00",
+                isFavorite = false
+            ),
+            DomainVulnOverview(
+                title = "Fatcat Apps の WordPress 用 analytics cat におけるクロスサイトリクエストフォージェリの脆弱性",
+                link = "https://jvndb.jvn.jp/ja/contents/2022/JVNDB-2022-020609.html",
+                description = "Fatcat Apps の WordPress 用 analytics cat には、クロスサイトリクエストフォージェリの脆弱性が存在します。",
+                id = "JVNDB-2022-020609",
+                issued = "2023-11-02T17:05:59+09:00",
+                modified = "2023-11-02T17:05:59+09:00",
+                isFavorite = false
+            ),
+            DomainVulnOverview(
+                title = "Pojo Me Digital LTD. の WordPress 用 Activity Log における CSV ファイル内の数式要素の中和に関する脆弱性",
+                link = "https://jvndb.jvn.jp/ja/contents/2022/JVNDB-2022-020608.html",
+                description = "Pojo Me Digital LTD. の WordPress 用 Activity Log には、CSV ファイル内の数式要素の中和に関する脆弱性が存在します。",
+                id = "JVNDB-2022-020608",
+                issued = "2023-11-02T17:05:58+09:00",
+                modified = "2023-11-02T17:05:58+09:00",
+                isFavorite = false
+            )
+        )
+
+        dataSource = FakeJvnDataSource(
+            initialSavedLocalVulnOverviews,
+            stubRemoteVulnOverviews
+        )
+    }
+
+
     @Test
     fun refreshVulnOverviews_getVulnOverviewsFailed_ReturnsResultFailure() = runTest {
         val mock = mock<JvnDataSource> {
@@ -44,5 +124,23 @@ class DefaultJvnRepositoryTest {
 
         assertTrue(result.isFailure)
         assertThat(result.exceptionOrNull().toString(), `is`(Exception("error").toString()))
+    }
+
+    @Test
+    fun refreshVulnOverviews_getVulnOverviewsSucceeded_LocalDataUpdated() = runTest {
+        val repository = DefaultJvnRepository(dataSource, dataSource)
+
+        var resultLocalData: List<DomainVulnOverview> = listOf()
+        val job = launch(UnconfinedTestDispatcher()) {
+            repository.vulnOverviews.collect {
+                resultLocalData = it
+            }
+        }
+
+        val res = repository.refreshVulnOverviews()
+        assertTrue(res.isSuccess)
+        assertThat(resultLocalData, `is`(stubRemoteVulnOverviews))
+
+        job.cancel()
     }
 }
